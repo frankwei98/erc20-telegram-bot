5ad6a44e2493080dd2e0f266e88be627de73686d
韦海晖 <franciswei98@gmail.com>
2019-11-26 22:40:46 +0800

    fix: no need to launch since we react to webhook

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,13 +8,11 @@ import commands from "./commands";
 
 const bot = new Telegraf(functions.config().bot.key);
 
-bot.hears('hi', (ctx) => ctx.reply('Hey there from Google Firebase Cloud Function'));
-bot.launch().then(() =>
-    console.log('Yay The bot just started')
-).catch(() => {
-    console.error('Oh No Something bad happened')
-});
+bot.start(ctx => {
+    ctx.reply(`欢迎使用 FrankCoin 机器人`).catch()
+})
 
+bot.hears('hi', (ctx) => ctx.reply('Hey there from Google Firebase Cloud Function'));
 commands(bot)
 
 export const webhookCall = functions.https.onRequest((req, res) => {
