056deaf932f18ac6c9de0540211a956c21e5769e
韦海晖 <franciswei98@gmail.com>
2019-12-06 17:25:11 +0800

    feat: add bot error catcher

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,19 +2,20 @@ import * as functions from 'firebase-functions';
 import Telegraf from "telegraf";
 import commands from "./commands";
 
-
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
 const bot = new Telegraf(functions.config().bot.key);
-
-bot.start(ctx => {
-    ctx.reply(`欢迎使用 FrankCoin 机器人`).catch()
+bot.catch((err :any, ctx: any) => {
+    console.log(`Ooops, ecountered an error for ${ctx.updateType}`, err)
 })
 
+bot.start((ctx) => ctx.reply('Hey there'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there from Google Firebase Cloud Function'));
 commands(bot)
 
-export const webhookCall = functions.https.onRequest((req, res) => {
-    bot.handleUpdate(req.body, res).catch(err => console.error(err));
+export const hook = functions.https.onRequest((req, res) => {
+    console.log(req.body)
+    bot.handleUpdate(req.body, res)
+        .catch(err => console.error(err));
 })
